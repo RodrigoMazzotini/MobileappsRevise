9331f959ffc972e044ce4a1f12af06c95136b56a
RodrigoMazzotini <MAZ23006145@heartofyorkshire.ac.uk>
2025-03-20 10:33:04 +0000

    colored app setup

--- ProdName/App.js
+++ ProdName/App.js
@@ -3,18 +3,33 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.header}>
+      <Text style ={styles.text}>ProdName</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  header: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#4B0907',
     alignItems: 'center',
     justifyContent: 'center',
+    height:100,
   },
+  text: {
+    color:'#FFCFAD',
+    fontSize:'40px',
+    fontweight: 'bold',
+
+
+  }
+
+
 });
+// #FFCFAD
+// #4B0907
+// #C95603
+// #F57200
+// #F4442E
