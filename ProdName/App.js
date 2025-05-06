c04b5d3fd183633e393e0a5c8174e99cb2adf339
RodrigoMazzotini <MAZ23006145@heartofyorkshire.ac.uk>
2025-05-06 11:07:39 +0000

    style

--- ProdName/App.js
+++ ProdName/App.js
@@ -3,33 +3,7 @@ import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert, StatusBar }
 import React, { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
-  return (
-    <View style={styles.header}>
-      <Text style ={styles.text}>ProdName</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  header: {
-    flex: 1,
-    backgroundColor: '#4B0907',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height:100,
-  },
-  text: {
-    color:'#FFCFAD',
-    fontSize:'40px',
-    fontweight: 'bold',
-
-
-  }
-
-
-});
 // #FFCFAD
 // #4B0907
 // #C95603
